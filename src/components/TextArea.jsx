4d5bc783fc76b47ab85926901ff9d465afbfe2ca
Gilson-SR <gilsonsrodrigues87@gmail.com>
2022-08-23 19:26:59 -0300

    Correção do componente TextArea

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -3,13 +3,12 @@ import PropTypes from 'prop-types';
 
 class TextArea extends React.Component {
   render() {
-    const { htmlText, name, type, value, onChange, dataTestId } = this.props;
+    const { htmlText, name, value, onChange, dataTestId } = this.props;
 
     return (
       <div>
         <label htmlFor={ name }>{htmlText}</label>
         <textarea
-          type={ type }
           name={ name }
           value={ value }
           onChange={ onChange }
@@ -25,7 +24,6 @@ export default Inputs;
 TextArea.propTypes = {
   htmlText: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
   dataTestId: PropTypes.string.isRequired,
