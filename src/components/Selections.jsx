1414900e58ef1a84eb82e6b4fb1e99d6b95864f9
Gilson-SR <gilsonsrodrigues87@gmail.com>
2022-08-23 19:33:06 -0300

    Criação componente Selections

--- src/components/Selections.jsx
+++ src/components/Selections.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-class Selections extends React.Component{
-    render(){
-        return(
-            <div>
-
-            </div>
-        );
-    }
+class Selections extends React.Component {
+  render() {
+    const { htmlText, name, value, onChange, dataTestId } = this.props;
+    return (
+      <div>
+        <label htmlFor={name}>{htmlText}</label>
+        <select
+          name={name}
+          value={value}
+          onChange={onChange}
+          dataTestId={dataTestId}
+        >
+          <option value="normal">Normal</option>
+          <option value="raro">Raro</option>
+          <option value="muito raro">Muito Raro</option>
+        </select>
+      </div>
+    );
+  }
 }
 
 export default Selections;
 
 Selections.propTypes = {
-
-}
+  htmlText: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  dataTestId: PropTypes.string.isRequired,
+};
