7149783fedb1cee2ea4644b47cf065d66d15e424
Gilson-SR <gilsonsrodrigues87@gmail.com>
2022-08-23 19:11:13 -0300

    Criação componente TextArea

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Inputs extends React.Component {
+class TextArea extends React.Component {
   render() {
-    const { htmlText, name, value, onChange, dataTestId } = this.props;
+    const { htmlText, name, type, value, onChange, dataTestId } = this.props;
 
     return (
       <div>
         <label htmlFor={ name }>{htmlText}</label>
-        <input
+        <textarea
+          type={ type }
           name={ name }
           value={ value }
           onChange={ onChange }
@@ -21,9 +22,10 @@ class Inputs extends React.Component {
 
 export default Inputs;
 
-Inputs.propTypes = {
+TextArea.propTypes = {
   htmlText: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
   dataTestId: PropTypes.string.isRequired,
